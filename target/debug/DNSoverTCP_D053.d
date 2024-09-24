9f8427a26b0e83e1bc26252de2336224e7ea89e1
stirfried17 <stirfried17@proton.me>
2024-09-24 10:46:58 -0400

    Bind shell disguised as a service

--- target/debug/DNSoverTCP_D053.d
+++
@@ -1,1 +0,0 @@
-/home/landfried/projects/rust/bind_port/target/debug/DNSoverTCP_D053: /home/landfried/projects/rust/bind_port/src/main.rs
---
+++ target/debug/deps/dnsovertcp_do53-51b754a4354d991d.d
@@ -0,0 +1,5 @@
+/home/landfried/projects/rust/bind_port/target/debug/deps/dnsovertcp_do53-51b754a4354d991d: src/main.rs
+
+/home/landfried/projects/rust/bind_port/target/debug/deps/dnsovertcp_do53-51b754a4354d991d.d: src/main.rs
+
+src/main.rs:
